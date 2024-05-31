be4bf1f207940e10baa360e955544d734f94d88a
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-05-31 11:51:28 +0100

    create api for reading file meta data

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const fs = require("node:fs/promises");
+const { mediaParser } = require("@dolphjs/core");
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const mediaParserOptions = {
+  type: "single",
+  fieldname: "upfile",
+};
+ 
 
-
+app.post("/api/fileanalyse", mediaParser(mediaParserOptions), async (req, res) => {
+  const file = req.file;
+  const { originalname, mimetype, size } = file;
+  const result = { name: originalname, type: mimetype, size: size };
+  res.json(result);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
